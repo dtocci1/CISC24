cc9e8238d295dba704a699c9bdea0fefbf52cc95
dtocci1 <43154755+dtocci1@users.noreply.github.com>
2020-02-28 10:15:15 -0500

    Laid out basics

--- CISC24/CISC24.srcs/sources_1/new/TopLevel.vhd
+++ CISC24/CISC24.srcs/sources_1/new/TopLevel.vhd
@@ -1,39 +1,27 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
 -- Create Date: 02/28/2020 09:52:04 AM
--- Design Name: 
 -- Module Name: TopLevel - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
+-- Project Name: CISC24
+-- Target Devices: Nexys-4
+-- Description:  
+--  This file will synthesize all the units of the CISC24 architecture together
+-- Dependencies: STD_LOGIC_1164, NUMERIC_STD
+-- Revision: 0.01
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use IEEE.NUMERIC_STD.ALL;
 
 entity TopLevel is
---  Port ( );
+  Port ( 
+    CLK : in std_logic;
+    RST : in std_logic;
+    result : out std_logic_vector(7 downto 0) -- unknown output size, temp 8-bits
+  );
 end TopLevel;
+
 
 architecture Behavioral of TopLevel is
 
